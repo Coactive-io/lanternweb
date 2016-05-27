51c6c648d21204f4c1c97f462437e7399c7e8104
George Haskell <george@egroeg.com>
2016-05-27 11:17:49 -0500

    Create custom 500 error page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+@section('title')
+	Lantern &dash; Go out. Discover your city.
+@stop
+
+@section('page_title')
+	<h1>We suck.  Sorry.</h1>
+	@stop
+
+@section('content')
+
+	<!-- content -->
+	<div class="row">
+		<div class="content col-md-offset-2 col-md-8">
+			<div class="main-text">
+				<p>Something's not quite right.  We're not sure what happened but we're working on making it right.</p>
+				<a href="/" class="nav-button">Go back home</a>
+			</div>
+		</div>
+	</div>
+	<!-- end content -->
+@endsection
+@section('scripts')
+
+	@endsection
