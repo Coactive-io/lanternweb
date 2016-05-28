e6869005447f7a63238b3d16c489c85a980b5432
ghaskell <george@egroeg.com>
2016-05-28 13:55:07 -0500

    framework for content page.

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+@section('title')
+	Lantern &dash; Go out. Discover your city.
+@stop
+
+@section('page_title')
+	Content goes here.
+@stop
+
+@section('content')
+	Put content here.
+@endsection
+
+@section('scripts')
+@endsection
